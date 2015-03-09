e45cdd28ae511bb7a059deca315bdbd30e9f32c1
Matt Lee <thatismatt@gmail.com>
2015-03-09 07:38:17 +0000

    Echo "Dual/Single Screen Mode".

--- scripts/screen-reconfigure.sh
+++ scripts/screen-reconfigure.sh
@@ -10,10 +10,12 @@ then
     xrandr --output eDP1  --mode 3200x1800 --pos 0x0 --rate 60.0 \
            --output HDMI1 --mode 1920x1080 --pos 3200x0 --rate 60.0
     xinput set-prop "$DEVICE"  --type=float "Coordinate Transformation Matrix" 0.625 0 0 0 1 0 0 0 1
+    echo "Dual Screen Mode"
 else
     xrandr --output eDP1  --mode 3200x1800 --pos 0x0 --rate 60.0 \
            --output HDMI1 --off
     xinput set-prop "$DEVICE"  --type=float "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
+    echo "Single Screen Mode"
 fi
 
 ./background.sh
