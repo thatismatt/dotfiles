a87124de8896d4b1f112b68518c14d674e58d7ba
Matt Lee <thatismatt@gmail.com>
2015-06-03 21:01:08 +0100

    Fix client restore key binding.

--- _config/awesome/rc.lua
+++ _config/awesome/rc.lua
@@ -298,7 +298,7 @@ globalkeys = awful.util.table.join(
    awful.key({ modkey            }, "space",  function () awful.layout.inc(layouts, 1) end),
    awful.key({ modkey, "Shift"   }, "space",  function () awful.layout.inc(layouts, -1) end),
 
-   awful.key({ modkey, "Control" }, "n",      awful.client.restore),
+   awful.key({ modkey, "Control" }, "n",      function () awful.client.restore(mouse.screen) end),
 
    -- Prompt
    awful.key({ modkey            }, "r",      function () mypromptbox:run() end),
