915b5456ffd5a51f9e0a5db06edff8708c0f1923
Matt Lee <thatismatt@gmail.com>
2015-06-03 11:43:53 +0100

    Awesome key binding tweaks.

--- _config/awesome/rc.lua
+++ _config/awesome/rc.lua
@@ -320,24 +320,21 @@ globalkeys = awful.util.table.join(
    awful.key({ modkey }, "p", function () menubar.show() end)
 )
 
+function toggle_maximized (c)
+   c.maximized_horizontal = not c.maximized_horizontal
+   c.maximized_vertical   = not c.maximized_vertical
+end
+
 clientkeys = awful.util.table.join(
-   awful.key({ modkey, "Shift"   }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
-   awful.key({ altkey            }, "F4",     function (c) c:kill()                         end),
-   awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
+   awful.key({ altkey            }, "F4",     function (c) c:kill() end),
+   awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle),
    awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
-   awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
-   awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
-   awful.key({ modkey,           }, "n",
-      function (c)
-         -- The client currently has the input focus, so it cannot be
-         -- minimized, since minimized clients can't have the focus.
-         c.minimized = true
-   end),
-   awful.key({ modkey,           }, "m",
-      function (c)
-         c.maximized_horizontal = not c.maximized_horizontal
-         c.maximized_vertical   = not c.maximized_vertical
-   end)
+   awful.key({ modkey,           }, "o",      awful.client.movetoscreen),
+   awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop end),
+   awful.key({ modkey, "Shift"   }, "f",      function (c) c.fullscreen = not c.fullscreen end),
+   awful.key({ modkey, "Shift"   }, "n",      function (c) c.minimized = true end),
+   awful.key({ modkey, "Shift"   }, "m",      toggle_maximized),
+   awful.key({ modkey, "Shift"   }, "d",      function (c) debug_client = c end)
 )
 
 -- Compute the maximum number of digit we need, limited to 9
