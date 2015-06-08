b6084a69c77f3a668c2a55f6805114bbc69513dd
Matt Lee <thatismatt@gmail.com>
2015-06-08 16:20:18 +0100

    Fix flash in fullscreen mode.

--- _config/awesome/rc.lua
+++ _config/awesome/rc.lua
@@ -441,6 +441,8 @@ awful.rules.rules = {
      properties = { floating = true } },
    { rule = { class = "gimp" },
      properties = { floating = true } },
+   { rule = { class = "Plugin-container" }, -- flash fullscreen e.g. youtube
+     properties = { floating = true } },
    -- Set Firefox to always map on tags number 2 of screen 1.
    -- { rule = { class = "Firefox" },
    --   properties = { tag = tags[1][2] } },
