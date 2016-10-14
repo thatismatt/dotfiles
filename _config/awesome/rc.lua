13dabaaa92a666b42abbb6704ccab9376c829c40
Matt Lee <thatismatt@gmail.com>
2016-10-14 13:55:31 +0100

    Stop inkscape starting floating.

--- _config/awesome/rc.lua
+++ _config/awesome/rc.lua
@@ -645,6 +645,8 @@ awful.rules.rules = {
      properties = { floating = true } },
    { rule = { class = "gimp" },
      properties = { floating = true } },
+   { rule = { class = "Inkscape" },
+     properties = { floating = false } },
    { rule = { class = "Plugin-container" }, -- flash fullscreen e.g. youtube
      properties = { floating = true } },
    { rule = { name = "Google+ Hangouts is sharing your screen with plus.google.com." },
