3bd9f8318bd143ab1f75577b5f2c4ce46d6010d5
Matt Lee <thatismatt@gmail.com>
2016-10-23 11:34:49 +0100

    Update single-screen script to work on desktop.

--- scripts/screen-single.sh
+++ scripts/screen-single.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-xrandr --output eDP1 --mode 3200x1800 --pos 0x0 --rate 60.0 \
-       --output HDMI1 --off
+case "$HOSTNAME" in
 
-DEVICE="SYNAPTICS Synaptics Large Touch Screen"
-xinput set-prop "$DEVICE"  --type=float "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
+    tillee)
+
+        xrandr --output eDP1 --mode 3200x1800 --pos 0x0 --rate 60.0 \
+               --output HDMI1 --off
+
+        DEVICE="SYNAPTICS Synaptics Large Touch Screen"
+        xinput set-prop "$DEVICE"  --type=float "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
+
+        ;;
+
+    regee)
+
+        xrandr --output VGA-1 --mode 1024x768 --pos 0x0 \
+               --output HDMI-1 --off
+
+        ;;
+esac
