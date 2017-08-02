8a359bbe61d0fd5b56e97c0c99122f2ecddf14ea
Matt Lee <thatismatt@gmail.com>
2017-08-02 17:25:31 +0100

    Update urxvt font script to allow setting font, as well as size.

--- scripts/urxvt-font-size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-printf "\033]710;xft:VL Gothic:size=$1\007"
---
+++ scripts/urxvt-font.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FONT_NAME="M+ 1m"
+FONT_SIZE="11"
+
+while getopts "f:s:" OPT; do
+    case $OPT in
+        f)
+            FONT_NAME=$OPTARG
+            ;;
+        s)
+            FONT_SIZE=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+
+echo "Setting font - name: $FONT_NAME"
+echo "               size: $FONT_SIZE"
+
+printf "\033]710;xft:$FONT_NAME:size=$FONT_SIZE\007"
