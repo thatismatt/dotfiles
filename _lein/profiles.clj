28835932abdd93d7f443a211e70176e9bb83aedf
Matt Lee <thatismatt@gmail.com>
2018-11-09 15:23:00 +0000

    Add my lein profiles.

---
+++ _lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:figwheel {:open-file-command "figwheel-open"}
+        :plugins [[lein-pprint "1.1.2"]
+                  [lein-auto "0.1.3"]]
+        :dependencies [[com.cemerick/pomegranate "1.1.0"]
+                       [gnuplot "0.1.0"]]}}
