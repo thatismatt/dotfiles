1baf8e9b0c8e2318b96bfae98721a8eceb2bb6a7
Matt Lee <thatismatt@gmail.com>
2018-12-21 09:10:44 +0000

    Add usage information for the urxvt font script.

--- scripts/urxvt-font.sh
+++ scripts/urxvt-font.sh
@@ -3,21 +3,30 @@
 #FONT_NAME="M+ 1m"
 FONT_NAME="Iosevka"
 FONT_SIZE="14"
+USAGE=true
+
+usage() {
+    echo "Usage: $0 [-f FONT_NAME] [-s FONT_SIZE]"
+}
 
 while getopts ":f:s:" OPT; do
     case $OPT in
         f)
             FONT_NAME=$OPTARG
+            USAGE=false
             ;;
         s)
             FONT_SIZE=$OPTARG
+            USAGE=false
             ;;
         \?)
             echo "Invalid option: -$OPTARG"
+            usage
             exit 1
             ;;
         :)
             echo "Option -$OPTARG requires an argument."
+            usage
             exit 1
             ;;
     esac
@@ -25,5 +34,7 @@ done
 
 echo "Setting font - name: $FONT_NAME"
 echo "               size: $FONT_SIZE"
+
+$USAGE && usage
 
 printf "\033]710;xft:$FONT_NAME:size=$FONT_SIZE\007"
