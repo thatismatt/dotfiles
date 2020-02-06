2bc627fbe3f9ecc9a3e351867c10d6e8e899ad9f
Matt Lee <thatismatt@gmail.com>
2020-02-06 15:50:42 +0000

    Disable all my Clojure dependencies and plugins by default.

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -1,5 +1,11 @@
 {:user {:figwheel {:open-file-command "figwheel-open"}
-        :plugins [[lein-pprint "1.1.2"]
-                  [lein-auto "0.1.3"]]
-        :dependencies [[com.cemerick/pomegranate "1.1.0"]
-                       [gnuplot "0.1.0"]]}}
+        :plugins [;; [lein-pprint "1.1.2"]
+                  ;; [lein-auto "0.1.3"]
+                  ;; [lein-eftest "0.5.8"]
+                  ;; [venantius/yagni "0.1.7"]
+                  ;; [lein-ancient "0.6.15"]
+                  ]
+        :dependencies [;; [com.cemerick/pomegranate "1.1.0"]
+                       ;; [gnuplot "0.1.0"]
+                       ;; [vvvvalvalval/scope-capture "0.3.2"]
+                       ]}}
