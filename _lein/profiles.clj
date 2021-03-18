008cba24b9fd82a859ff5f084b6b28870b644c3d
Matt Lee <thatismatt@gmail.com>
2021-03-18 09:29:13 +0000

    Update lein pprint version.

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:repl-options {:init-ns user}
         :figwheel {:open-file-command "figwheel-open"}
-        :plugins [;; [lein-pprint "1.1.2"]
+        :plugins [;; [lein-pprint "1.3.2"]
                   ;; [lein-auto "0.1.3"]
                   ;; [lein-eftest "0.5.8"]
                   ;; [venantius/yagni "0.1.7"]
