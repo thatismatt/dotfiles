df70b96f4a7df1b8160deec518089cc82a0007b6
Matt Lee <thatismatt@gmail.com>
2022-10-05 11:36:43 +0100

    Turn off lein's pedantic abort.

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:repl-options {:init-ns user}
+        :pedantic? :ranges
         :figwheel {:open-file-command "figwheel-open"}
         :plugins [;; [lein-pprint "1.3.2"]
                   ;; [lein-auto "0.1.3"]
